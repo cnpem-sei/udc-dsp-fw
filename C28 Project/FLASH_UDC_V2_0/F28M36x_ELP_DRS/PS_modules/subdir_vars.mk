a334055a1b7e5105a8e448c827b06a8bef1909f3
gabrielbrunheira <gabrielbrunheira@gmail.com>
2016-07-26 14:46:41 -0300

    PS = FAC_ACDC / Test of Timeout functionality using CPU Timer 1

--- C28 Project/FLASH_UDC_V2_0/F28M36x_ELP_DRS/PS_modules/subdir_vars.mk
+++ C28 Project/FLASH_UDC_V2_0/F28M36x_ELP_DRS/PS_modules/subdir_vars.mk
@@ -4,27 +4,27 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-D:/ARQ/Projects/C28/F28M36x_ELP_DRS/PS_modules/FBP_100kHz.c \
+D:/ARQ/Projects/C28/F28M36x_ELP_DRS/PS_modules/FAP_ACDC.c \
 D:/ARQ/Projects/C28/F28M36x_ELP_DRS/PS_modules/PS_modules.c 
 
 OBJS += \
-./F28M36x_ELP_DRS/PS_modules/FBP_100kHz.obj \
+./F28M36x_ELP_DRS/PS_modules/FAP_ACDC.obj \
 ./F28M36x_ELP_DRS/PS_modules/PS_modules.obj 
 
 C_DEPS += \
-./F28M36x_ELP_DRS/PS_modules/FBP_100kHz.pp \
+./F28M36x_ELP_DRS/PS_modules/FAP_ACDC.pp \
 ./F28M36x_ELP_DRS/PS_modules/PS_modules.pp 
 
 C_DEPS__QUOTED += \
-"F28M36x_ELP_DRS\PS_modules\FBP_100kHz.pp" \
+"F28M36x_ELP_DRS\PS_modules\FAP_ACDC.pp" \
 "F28M36x_ELP_DRS\PS_modules\PS_modules.pp" 
 
 OBJS__QUOTED += \
-"F28M36x_ELP_DRS\PS_modules\FBP_100kHz.obj" \
+"F28M36x_ELP_DRS\PS_modules\FAP_ACDC.obj" \
 "F28M36x_ELP_DRS\PS_modules\PS_modules.obj" 
 
 C_SRCS__QUOTED += \
-"D:/ARQ/Projects/C28/F28M36x_ELP_DRS/PS_modules/FBP_100kHz.c" \
+"D:/ARQ/Projects/C28/F28M36x_ELP_DRS/PS_modules/FAP_ACDC.c" \
 "D:/ARQ/Projects/C28/F28M36x_ELP_DRS/PS_modules/PS_modules.c" 
 
 
