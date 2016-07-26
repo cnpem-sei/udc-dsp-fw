d4e5311bf93266da7990964fa3c545f140cdd15a
gabrielbrunheira <gabrielbrunheira@gmail.com>
2016-07-26 12:14:22 -0300

    NOT WORKING. Test of Timeout functionality using CPU Timer 0.

--- F28M36x_ELP_DRS/PS_modules/PS_modules.h
+++ F28M36x_ELP_DRS/PS_modules/PS_modules.h
@@ -29,15 +29,15 @@ typedef enum
 	OPEN_LOOP
 } ePSLoopState;
 
-#define PS_MODEL	FBP_100kHz
+#define PS_MODEL	FAP_ACDC
 
-#include "FBP_100kHz.h"
+//#include "FBP_100kHz.h"
 //#include "FBP_Parallel_100kHz.h"
 //#include "FAC_ACDC_10kHz.h"
 //#include "FAC_DCDC_20kHz.h"
 //#include "FAC_Full_ACDC_10kHz.h"
 //#include "FAC_Full_DCDC_20kHz.h"
-//#include "FAP_ACDC.h"
+#include "FAP_ACDC.h"
 //#include "FAP_DCDC_20kHz.h"
 //#include "Test_HRPWM.h"
 //#include "Test_HRADC.h"
