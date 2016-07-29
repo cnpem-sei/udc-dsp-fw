c3427200c027bf73f598533f019a30e9fef74bbf
gabrielbrunheira <gabrielbrunheira@gmail.com>
2016-07-29 11:02:16 -0300

    First commit of branch "ps_model"

---
+++ C28 Project/FLASH_UDC_V2_0/F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.pp
@@ -0,0 +1,77 @@
+# FIXED
+
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.c
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/math.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/linkage.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_common/include/DSP28x_Project.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Device.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Adc.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_AnalogSysCtrl.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Comp.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_CpuTimers.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_DevEmu.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Dma.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Ecap.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Epwm.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Eqep.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Flash.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Gpio.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_HWBist.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_I2c.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Ipc.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Mcbsp.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_NmiIntrupt.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_PieCtrl.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_PieVect.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Ram.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Sci.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Spi.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Sysctrl.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_XIntrupt.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_Examples.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_GlobalPrototypes.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_EPwm_defines.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_I2c_defines.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_Dma_defines.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_AnalogSysCtrl_defines.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_set_pinout.h
+F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.obj: D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_DefaultISR.h
+
+D:/ARQ/Projects/C28/F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.c: 
+D:/ARQ/Projects/C28/F28M36x_ELP_DRS/DP_framework/ELP_DCL/ELP_DCL.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/math.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.10/include/linkage.h: 
+D:/ARQ/Projects/C28/F28M36x_common/include/DSP28x_Project.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Device.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Adc.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_AnalogSysCtrl.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Comp.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_CpuTimers.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_DevEmu.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Dma.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Ecap.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Epwm.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Eqep.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Flash.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Gpio.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_HWBist.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_I2c.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Ipc.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Mcbsp.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_NmiIntrupt.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_PieCtrl.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_PieVect.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Ram.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Sci.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Spi.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Sysctrl.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_XIntrupt.h: 
+D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_Examples.h: 
+D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_GlobalPrototypes.h: 
+D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_EPwm_defines.h: 
+D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_I2c_defines.h: 
+D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_Dma_defines.h: 
+D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_AnalogSysCtrl_defines.h: 
+D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_set_pinout.h: 
+D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_DefaultISR.h: 
---
+++ C28 Project/FLASH_UDC_V2_0/source/F28M36x_Mcbsp.pp
@@ -0,0 +1,69 @@
+# FIXED
+
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_common/source/F28M36x_Mcbsp.c
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Device.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Adc.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_AnalogSysCtrl.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Comp.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_CpuTimers.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_DevEmu.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Dma.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Ecap.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Epwm.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Eqep.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Flash.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Gpio.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_HWBist.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_I2c.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Ipc.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Mcbsp.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_NmiIntrupt.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_PieCtrl.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_PieVect.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Ram.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Sci.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Spi.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Sysctrl.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_XIntrupt.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_Examples.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_GlobalPrototypes.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_EPwm_defines.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_I2c_defines.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_Dma_defines.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_AnalogSysCtrl_defines.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_set_pinout.h
+source/F28M36x_Mcbsp.obj: D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_DefaultISR.h
+
+D:/ARQ/Projects/C28/F28M36x_common/source/F28M36x_Mcbsp.c: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Device.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Adc.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_AnalogSysCtrl.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Comp.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_CpuTimers.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_DevEmu.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Dma.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Ecap.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Epwm.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Eqep.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Flash.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Gpio.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_HWBist.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_I2c.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Ipc.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Mcbsp.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_NmiIntrupt.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_PieCtrl.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_PieVect.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Ram.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Sci.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Spi.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_Sysctrl.h: 
+D:/ARQ/Projects/C28/F28M36x_headers/include/F28M36x_XIntrupt.h: 
+D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_Examples.h: 
+D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_GlobalPrototypes.h: 
+D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_EPwm_defines.h: 
+D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_I2c_defines.h: 
+D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_Dma_defines.h: 
+D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_AnalogSysCtrl_defines.h: 
+D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_set_pinout.h: 
+D:/ARQ/Projects/C28/F28M36x_common/include/F28M36x_DefaultISR.h: 
