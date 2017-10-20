b4b779542c7f5afeaf0b11fdc70f72e564d75afd
gabrielbrunheira <gabrielbrunheira@gmail.com>
2017-10-20 16:31:14 -0200

    Removed FLASH_UDC_V2_1 from working tree and index

--- FLASH_UDC_V2_1/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"/home/gabriel/lnls-elp/controlsuite/c28/F28M36x_headers/cmd/F28M36x_Headers_nonBIOS.cmd" -l"/home/gabriel/lnls-elp/controlsuite/c28/F28M36x_common/lib/SFO_TI_Build_V7_FPU.lib" -l"/home/gabriel/lnls-elp/controlsuite/c28/FPUfastRTS/V100/lib/rts2800_fpu32_fast_supplement.lib" -l"/home/gabriel/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/lib/rts2800_fpu32.lib" -l"/home/gabriel/ti/ccsv7/tools/compiler/ti-cgt-c2000_16.9.1.LTS/lib/libc.a"
-
